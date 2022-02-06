afa5808ad58a81f6d36003cd11e14c2371ac01a8
Rafael Ramirez <rafa.yael@gmail.com>
2022-02-06 09:34:43 -0600

    fix: Estructura mejorada. feat; Pages.components agregados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PagesComponent,
+    loadChildren: () =>
+      import('./pages/pages.module').then((m) => m.PagesModule),
+  },
+
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
-import { DarkReaderService } from './services/dark-reader.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'documentos-vaticanos';
-
-  constructor(public darkModeService: DarkReaderService) {}
-
-  temaClaro = true;
-  tema(claro = this.temaClaro) {
-    console.log({ claro });
-    if (!claro) this.darkModeService.dark();
-    else this.darkModeService.disable();
-  }
+ 
 }
