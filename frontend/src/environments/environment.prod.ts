963c75d4706910e0ba5ea216db8b9e4e150d25ae
Mr. Nochon <mr.nochon@codice-progressio.online>
2026-07-25 14:57:11 +0000

    chore(release): 0.0.21

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  version: '0.0.20',
+  version: '0.0.21',
   apiBaseUrl: '', // set at deploy or leave empty to disable online features
   devAuthBypass: false,
   googleClientId: '',
